9441cefc7989f128acb6b7ae6aad841ad42cda03
virgil <virgil@softix.nl>
2024-12-01 23:50:10 +0100

    Add cicleshape.py

--- python/asteroids/main.py
+++ python/asteroids/main.py
@@ -10,6 +10,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -29,6 +32,7 @@ def main():
         sky_rect = pygame.Surface.fill(sky_surface, (255, 0, 0))
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     pygame.quit()
 
